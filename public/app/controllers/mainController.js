7638c01d3a86acb3433a19bd4181e8081033d220
agcrs <agcrst@gmail.com>
2016-03-08 16:05:45 +0100

    MERGE createServer->master
    Branch createServer merged into master.

---
+++ public/app/controllers/mainController.js
@@ -0,0 +1,56 @@
+//===============================================
+//This controller is applied to the overall layout
+//of the application.
+//It is responsible of holding the logged in and
+//logged out user information.
+//==============================================
+
+angular.module('mainController', [])
+
+.controller('mainController', function($rootScope, $location, Auth) {
+
+    var vm = this;  //Set up the view-model
+
+    /*vm.loggedIn = Auth.isLoggedIn();    //Get info if someone is logged in
+
+    //Check if the user is logged in on every request
+    $rootScope.$on('$routeChangeStart', function()  {  //Detect a router change
+
+        vm.loggedIn = Auth.isLoggedIn();
+
+        Auth.getUser() //get user information on route change
+            .success(function(data) {
+                vm.user = data;
+            });
+
+    });
+
+    //Function to handle login form
+    vm.doLogin = function() {
+
+        vm.processing = true;
+        vm.error = ""; //Clear the error
+
+        Auth.login(vm.loginData.username, vm.loginData.password)
+            .success(function(data) {
+
+                vm.processing = false;
+
+                if (data.success){
+                    $location.path('/users');   //If the user logs in, redirect
+                } else {
+                    vm.error = data.message;
+                }
+
+
+            });
+    };
+
+    //Function to handle loggin out
+    vm.doLogout = function()    {
+        Auth.logout();
+        vm.user = {};
+        $location.path('/login');
+    };*/
+
+});
---
+++ public/app/services/authService.js
@@ -0,0 +1,113 @@
+angular.module('authService', [])
+
+//===============================================
+//Factory to handle tokens
+//inject $window to store token client-side
+//===============================================
+
+.factory('AuthToken', function($window) {
+
+    var authTokenFactory = {};
+
+    //get the token
+    authTokenFactory.getToken = function()  {
+        return $window.localStorage.getItem('token');
+    };
+
+    //set the token or clear the token
+    authTokenFactory.setToken = function(token) {
+        if (token){
+            $window.localStorage.setItem('token', token);
+        } else {
+            $window.localStorage.removeItem('token');
+        }
+    };
+
+    return authTokenFactory;
+})
+
+//===============================================
+//Auth factory to login and get information
+//inject $http for communicating with the Api
+//inject $q to return promise objects
+//inject AuthToken to manage tokens
+//===============================================
+
+.factory('Auth', function($http, $q, AuthToken) {
+
+    var authFactory = {};
+
+    //handle login
+    authFactory.login = function(username, password) {
+
+        //return the promise object an its data
+        return $http.post('/api/authenticate', {
+            username: username,
+            password: password
+        })
+            .success(function(data) {
+                AuthToken.setToken(data.token);
+                return data;
+            });
+    };
+
+    //handle logout
+    authFactory.logout = function() {
+        //clear the token
+        AuthToken.setToken();
+    };
+
+    //chek if a user is logged in
+    authFactory.isLoggedIn = function() {
+        if (AuthToken.getToken()) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    //get user info
+    authFactory.getUser = function() {
+		if (AuthToken.getToken()) {
+            return $http.get('/api/me', { cache: true });
+        } else {
+			return $q.reject({ message: 'User has no token.' });
+        }
+	};
+
+    return authFactory;
+})
+
+//===============================================
+//Application configuration to integrate token
+//into requests.
+//===============================================
+
+.factory('AuthInterceptor', function($q, $location, AuthToken) {
+
+    var interceptorFactory = {};
+
+    //attach the token to every request
+    interceptorFactory.request = function(config)   {
+        var token = AuthToken.getToken();
+
+        if (token) {
+            config.headers['x-access-token'] = token;
+        }
+
+        return config;
+    };
+
+    //redirect if a token doesn't authenticate
+    interceptorFactory.responseError = function(response)   {
+
+        if(response.status == 403){ //If server returns forbidden response
+            AuthToken.setToken();   //Remove the tokens
+            $location.path('/login');   //Redirect to login
+        }
+
+        return $q.reject(response); //Return the error from the server as a promise
+    };
+
+    return interceptorFactory;
+});
