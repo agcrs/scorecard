4c0a8078ebf33ec6e03526e92cf075dbd8602535
agcrs <agcrst@gmail.com>
2016-03-08 16:01:00 +0100

    BASIC SERVER
    Basic node express server with welcome view implemented

--- public/app/app.js
+++ public/app/app.js
@@ -1,11 +1,12 @@
 angular.module('scorecardApp', [
     'ngAnimate',        //Used to add animations to ngShow and ngHide mainly
-    'app.routes'       //The routing of the application
-    //'mainController'   //Controller for the main view
-]);
+    'app.routes',       //The routing of the application
+    'authService',      //Service to manage authorization
+    'mainController',   //Controller for the main view
+])
 
 //application configuration to integrate token into requests
-//.config(function($httpProvider) {
+.config(function($httpProvider) {
     //attach our auth interceptor to the http requests
-    //$httpProvider.interceptors.push('AuthInterceptor');
-//});
+    $httpProvider.interceptors.push('AuthInterceptor');
+});
