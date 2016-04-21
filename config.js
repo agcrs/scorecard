29e61797e27897eb877adcaf8654944132243a37
agcrst <agcrst@gmail.com>
2016-04-21 20:02:08 +0200

    Heroku config trial3

--- config.js
+++ config.js
@@ -8,6 +8,6 @@ module.exports = {
         'clientID'      : '919871325061-7jvhm24c0i6eu7f7133fb203si21akh4.apps.googleusercontent.com',
         'clientSecret'  : 'XYZ5DP4PxXSlpMf5uFyvpJnO',
         //'callbackURL'   : 'http://localhost:8080/auth/google/callback',
-        'callbackURL'   : 'https://scorecard16.herokuapp.com//auth/google/callback'
+        'callbackURL'   : 'https://scorecard16.herokuapp.com/auth/google/callback'
     }
 };
