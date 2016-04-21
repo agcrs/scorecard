c363671869be48741eff41916cb51f4246426ae0
agcrst <agcrst@gmail.com>
2016-04-21 20:00:49 +0200

    Heroku config trial2

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ module.exports = {
     'googleAuth': {
         'clientID'      : '919871325061-7jvhm24c0i6eu7f7133fb203si21akh4.apps.googleusercontent.com',
         'clientSecret'  : 'XYZ5DP4PxXSlpMf5uFyvpJnO',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        //'callbackURL'   : 'http://localhost:8080/auth/google/callback',
+        'callbackURL'   : 'https://scorecard16.herokuapp.com//auth/google/callback'
     }
 };
