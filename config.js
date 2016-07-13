21570ffcc2e04e31f0a096a3c948579702d5bc82
agcrst <agcrst@gmail.com>
2016-07-13 17:13:31 +0200

    Fixed error in config.

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     'port': process.env.PORT || 8080,
-    'database' : 'mongodb://localhost:27017/scorecard',
+    //'database' : 'mongodb://localhost:27017/scorecard',
     'database' : 'mongodb://mlab:383137@ds013310.mlab.com:13310/scorecard',
-    //'secret' : '20152016tfg',
+    'secret' : '20152016tfg',
 
     'googleAuth': {
         'clientID'      : '919871325061-7jvhm24c0i6eu7f7133fb203si21akh4.apps.googleusercontent.com',
